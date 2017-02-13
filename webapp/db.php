8f5fc7e9d0b2bcf7f7f0eeaf52ba26c5fc7f812e
yinashh <yina.shang@outlook.com>
2017-02-13 15:50:42 -0800

    reoarganized files

--- webapp/db.php
+++ webapp/db.php
@@ -7,5 +7,8 @@ $connection = mysqli_connect('localhost', 'root', '', 'loginapp');
 if (!$connection) {
     die("Database connection failed");
 }
-
+// connect to AWS database
+// server: traffic-vision-master.comcdsygcrhe.us-east-1.rds.amazonaws.com
+// username: tvvancouver
+// password: not sure ('' or covcp2017 maybe)
 ?>
